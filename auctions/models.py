56c4eb0e2fff50eb93e02b99c7cbffeb49d4d1f7
Keith Matthew Averre Jr <kmaverre@alaska.edu>
2022-10-23 07:19:49 -0800

    Adding model comments to implement

--- auctions/models.py
+++ auctions/models.py
@@ -17,14 +17,21 @@ TODO:
         -Description
         -user who made it
         -categories
-        -starting bid allowed
-
+        -current bid --> starting bid at creation
+        -Comments
 """
 
 """
 MODEL: bid for listing
 
 TODO:
+    -add fields:
+        -User who is bidding
+        -bid amount
+        -parent listing
+        
+    RELATIONSHIP: many to one with listing
+        many bids per listing but only one listing per bid
 
 """
 
@@ -32,5 +39,19 @@ TODO:
 MODEL: Comments for listing
 
 TODO:
+    -add fields:
+        -User who is commenting
+        -text of comment
+        
+    -possible fields:
+        -possible parent listing
+        -possible parent comment
+        
+    
+    RELATIONSHIP: many to one with listing
+        many comments per listing but only one listing per comment
+        
+                  many to zero or one with comment
+        many comments per parent comment but only zero or one parent comment per comment
 
 """
