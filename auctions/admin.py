d106965a7f80aaa3607771b2b154c10fa2655f70
KeithAverre <kmaverre@alaska.edu>
2022-10-24 09:58:15 -0800

    Registering models into admin panel

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Listing, bid, Comment,User
 
+admin.site.register(User)
+admin.site.register(Listing)
+admin.site.register(bid)
+admin.site.register(Comment)
 # Register your models here.
