99edd9d9c5d5739e65f600cb0186c6cca725b513
Keith Matthew Averre Jr <kmaverre@alaska.edu>
2022-11-03 09:55:53 -0800

    Comments for javascript

--- auctions/static/auctions/watchlist_toggle.js
+++ auctions/static/auctions/watchlist_toggle.js
@@ -1,4 +1,5 @@
 if(document.querySelector('#watchlist_count') != null) {
+    //grabs watchlist count
     fetch("/api_watchlist_count", {method: "GET"})
         .then((response => response.json()))
         .then(data => {
@@ -9,18 +10,25 @@ if(document.querySelector('#watchlist_count') != null) {
         })
 
     document.addEventListener('DOMContentLoaded', () => {
-
+        //waits for a favorite button to be pressed
         let listing_id = 0;
         document.querySelectorAll('#favorite_button').forEach(button => {
             button.onclick = function () {
+                //set a data-id field for each button to then grab the listing_id
                 listing_id = button.dataset.id;
                 console.log(listing_id);
+
+                //allows each favorite button to be individually addressable
                 fetch(`/api_watchlist_toggle/${listing_id}`, {method: "POST"})
                     .then(response => response.json())
                     .then(data => {
                         //thank you to https://abcdinamo.com/news/using-variable-fonts-on-the-web
+                        //isInWatchlist is a boolean value, as the name suggests, it is true if the listing is in favorites, and false if not.
                         document.querySelector(`#heart${listing_id}`).style.fontVariationSettings = "\"FILL\" " + data['isInWatchlist'];
-                        document.querySelector('#watchlist_count').innerHTML = "Watchlist [" + data['watchlist_count'] + "]"
+
+                        //format for showing count on watchlist
+                        document.querySelector('#watchlist_count').innerHTML = "Watchlist [" + data['watchlist_count'] + "]";
+
                     })
                     .catch(error => {
                         console.log('**Error**', error)
