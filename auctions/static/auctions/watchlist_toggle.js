4e296a42b39a54e1a3f4081b60636cea4e7dc5ef
Keith Matthew Averre Jr <kmaverre@alaska.edu>
2022-11-03 02:49:30 -0800

    fully implemented a working favorite button for each listing

--- auctions/static/auctions/watchlist_toggle.js
+++ auctions/static/auctions/watchlist_toggle.js
@@ -4,11 +4,13 @@ fetch("/api_watchlist_count",{method:"GET"})
     .then(data =>{
         document.querySelector('#watchlist_count').innerHTML = "Watchlist [" + data['watchlist_count'] + "]"
     })
+    .catch(error =>{
+        console.log('**Error**',error)
+    })
 
  document.addEventListener('DOMContentLoaded', ()=> {
 
         let listing_id = 0;
-        //if(document.querySelector('#watchlist_toggle') != null) {
         document.querySelectorAll('#favorite_button').forEach(button => {
             button.onclick = function () {
                 listing_id = button.dataset.id;
@@ -17,7 +19,7 @@ fetch("/api_watchlist_count",{method:"GET"})
                     .then(response => response.json())
                     .then(data =>{
                     //thank you to https://abcdinamo.com/news/using-variable-fonts-on-the-web
-                    document.querySelector('.material-symbols-outlined').style.fontVariationSettings = "\"FILL\" " + data['isInWatchlist'];
+                    document.querySelector(`#heart${listing_id}`).style.fontVariationSettings = "\"FILL\" " + data['isInWatchlist'];
                     document.querySelector('#watchlist_count').innerHTML = "Watchlist [" + data['watchlist_count'] + "]"
                 })
                     .catch(error =>{
